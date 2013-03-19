4c86f9843b54c62b33c9e81596d764c5122fdb5f
Computer Engineering <cn@ComClass.(none)>
2013-03-19 19:42:59 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMail::Application.config.secret_token = '5d27736ff36b6251c899950f6efa2cda519b1f1e413f4f044f5c86b329ec601d998293df0aa18391e22a3ec94707cf6bff46a54a9fd843646cf9bcb6877c8ba7'
